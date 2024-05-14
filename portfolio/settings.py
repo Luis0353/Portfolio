9df028fd03219c6a04f1f6d405b38c91baa7f7d2
Luis Ruiz Peña <loohis033@gmail.com>
2024-05-14 18:36:02 -0400

    Modificando variables de entorno

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,22 +11,24 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g)c!6^mz_$r(+-xfqlif$kz4+2!_zh)rk@^)4^xk9+ilu^tr(t'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default = False)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default = [])
 
 # Application definition
 
