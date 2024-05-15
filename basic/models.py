ecfb6d080d7e900d81b1bc086aaf9c1f65f216dc
Luis Ruiz Peña <loohis033@gmail.com>
2024-05-15 11:35:18 -0400

    Primer Modelo

--- basic/models.py
+++ basic/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    repository = models.URLField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
